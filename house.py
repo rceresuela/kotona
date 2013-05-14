c8ab7db684490132f5bbdf4e18d4f4a517848643
raul <raul.ceresuela@gmail.com>
2013-05-14 23:11:04 +0300

    first commit

---
+++ house.py
@@ -0,0 +1,7 @@
+class House:
+	def __init__(self, address, size, prize, rooms, room_configuration):
+		self.address = address
+		self.size = size
+		self.prize = prize
+		self.rooms = rooms
+		self.room_configuration = room_configuration
